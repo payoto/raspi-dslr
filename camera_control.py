448231817bd161414ff1ce7368367f245de8705e
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-04-17 01:25:49 +0200

    Fix issues and add command line control args

--- camera_control.py
+++ camera_control.py
@@ -1,8 +1,10 @@
 from time import sleep
 from datetime import datetime
 from sh import gphoto2 as gp
-import signal, os, subprocess
-
+import signal
+import os
+import subprocess
+import argparse
 # Kill the gphoto process that starts
 # whenever we turn on the camera or
 # reboot the raspberry pi
@@ -18,32 +20,29 @@ def killGphoto2Process():
             pid = int(line.split(None,1)[0])
             os.kill(pid, signal.SIGKILL)
 
-# shot_date = datetime.now().strftime("%Y-%m-%d") # This has been written to the while True loop.
-# shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # This has been written to the while True loop.
-picID = "PiShots"
 
-clearCommand = ["--folder", "/store_00020001/DCIM/100CANON", \
+clearCommand = ["--folder", "/store_00020001/DCIM/100CANON",
                 "--delete-all-files", "-R"]
 triggerCommand = ["--trigger-capture"]
 downloadCommand = ["--get-all-files"]
 
-folder_name = shot_date + picID
-save_location = "/home/pi/Desktop/gphoto/images/" + folder_name
 
-def createSaveFolder():
+def createSaveFolder(save_location):
     try:
         os.makedirs(save_location)
     except:
         print("Failed to create new directory.")
     os.chdir(save_location)
 
-def captureImages():
+
+def captureImages(delay=3):
     gp(triggerCommand)
-    sleep(3)
+    sleep(delay)
     gp(downloadCommand)
     gp(clearCommand)
 
-def renameFiles(ID):
+
+def renameFiles(ID, shot_time):
     for filename in os.listdir("."):
         if len(filename) < 13:
             if filename.endswith(".JPG"):
@@ -54,13 +53,29 @@ def renameFiles(ID):
                 print("Renamed the CR2")
 
 
-killGphoto2Process()
-gp(clearCommand)
+def main():
+    parser = argparse.ArgumentParser(description='Control a time lapse.')
+    parser.add_argument('-H', '--hours', type=float, default=12.0,
+                        help='the duration of the timelapse in hours')
+    parser.add_argument('-P', '--period', default=30.0, type=float,
+                        help='Period in seconds between pictures')
+    parser.add_argument('-d', '--directory', default="PiShots",
+                        type=str, help='Name of the output directory')
 
-while True:
-	shot_date = datetime.now().strftime("%Y-%m-%d")
-	shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	createSaveFolder()
-	captureImages()
-	renameFiles(picID)
+    args = parser.parse_args()
+    killGphoto2Process()
+    gp(clearCommand)
+    num_pictures = int(args.hours*3600 / args.period)
 
+    shot_date = datetime.now().strftime("%Y-%m-%d")
+    shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    folder_name = args.directory + shot_date
+    save_location = "~/gphoto/images/" + folder_name
+    createSaveFolder(save_location)
+    for i in range(num_pictures):
+        captureImages(args.period)
+        renameFiles(args.directory, shot_time)
+
+
+if __name__ == '__main__':
+    main()
