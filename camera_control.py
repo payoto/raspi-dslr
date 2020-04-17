e6b318b2274324c9c6406091104792d891a725f1
Alexandre Payot <alexandre.payotli@gmail.com>
2020-04-17 13:51:11 +0200

    Improve functionality

--- camera_control.py
+++ camera_control.py
@@ -20,11 +20,21 @@ def killGphoto2Process():
             pid = int(line.split(None,1)[0])
             os.kill(pid, signal.SIGKILL)
 
+try:
+    camera_base_store = re.compile(
+        "/store[^'\n]*DCIM"
+    ).findall(str(a.stdout))[0]
+except Exception as e:
+    camera_base_store = '/store_00010001/DCIM/'
 
-clearCommand = ["--folder", "/store_00020001/DCIM/100CANON",
-                "--delete-all-files", "-R"]
+camera_subfolder = "101TLAPS"
+
+def clearFolder(folder): 
+    return ["--folder", folder, "--delete-all-files", "-R"]
 triggerCommand = ["--trigger-capture"]
-downloadCommand = ["--get-all-files"]
+
+def downloadCommand(folder): 
+    return ["--folder", folder, "--get-all-files"]
 
 
 def createSaveFolder(save_location):
@@ -35,11 +45,11 @@ def createSaveFolder(save_location):
     os.chdir(save_location)
 
 
-def captureImages(delay=3):
+def captureImages(folder=camera_base_store + camera_subfolder, delay=3):
     gp(triggerCommand)
     sleep(delay)
-    gp(downloadCommand)
-    gp(clearCommand)
+    gp(downloadCommand(folder))
+    gp(clearFolder(folder))
 
 
 def renameFiles(ID, shot_time):
@@ -47,10 +57,10 @@ def renameFiles(ID, shot_time):
         if len(filename) < 13:
             if filename.endswith(".JPG"):
                 os.rename(filename, (shot_time + ID + ".JPG"))
-                print("Renamed the JPG")
-            elif filename.endswith(".CR2"):
-                os.rename(filename, (shot_time + ID + ".CR2"))
-                print("Renamed the CR2")
+                print(f"Renamed the {filename}")
+            elif filename.endswith(".NEF"):
+                os.rename(filename, (shot_time + ID + ".NEF"))
+                print(f"Renamed the {filename}")
 
 
 def main():
@@ -61,20 +71,33 @@ def main():
                         help='Period in seconds between pictures')
     parser.add_argument('-d', '--directory', default="PiShots",
                         type=str, help='Name of the output directory')
+    parser.add_argument('--maxexposure', default=2.0,
+                        type=float, help='Maximum mexposure in seconds')
 
     args = parser.parse_args()
+    
+    if args.period < args.maxexposure:
+        ArgumentError(
+        f"--maxexposure ({args.maxexposure}) cannot be above the period" 
+        + f" ({args.period})")
+    
     killGphoto2Process()
-    gp(clearCommand)
     num_pictures = int(args.hours*3600 / args.period)
 
     shot_date = datetime.now().strftime("%Y-%m-%d")
     shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    folder_name = args.directory + shot_date
-    save_location = "~/gphoto/images/" + folder_name
+    folder_name = args.directory + '-' + shot_date
+    save_location = "./timelapses/" + folder_name
+    camera_save_location = camera_base_store + camera_subfolder
+    
     createSaveFolder(save_location)
+    gp(clearFolder(camera_save_location))
     for i in range(num_pictures):
-        captureImages(args.period)
+        start_time = datetime.now()
+        captureImages(folder=camera_save_location,delay=args.maxexposure)
         renameFiles(args.directory, shot_time)
+        sleep(max(0.0,
+        args.period - (datetime.now() - start_time).total_seconds()))
 
 
 if __name__ == '__main__':
--- install_camera_control.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install gphoto2
-sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo pip3 install sh
